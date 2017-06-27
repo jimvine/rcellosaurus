f8446119b3950c1fce2af7873f642ea1ce3138d5
Jim Vine <code@jimvine.co.uk>
2017-06-27 13:36:51 +0100

    Handling multiple search terms

--- R/cellosaurus.R
+++ R/cellosaurus.R
@@ -31,6 +31,8 @@ read_cellosaurus_xml <- function(data) {
 #' @param text
 #'   Some text to search for. Finding is handled using XPath so some
 #'   special characters may cause difficulties, including these: '/:[]*.
+#'   Can take a character vector of length > 1, in which case will search for
+#'   any of the terms (i.e., "or").
 #'
 #' @return An XML node containing a cell-line (or an \code{xml_missing} node if
 #'   the search text is not found). If there are multiple matches, the first
@@ -46,9 +48,17 @@ read_cellosaurus_xml <- function(data) {
 #' @export
 #'
 cell_line_find_first <- function(cellosaurus, text) {
-  xml2::xml_find_first(cellosaurus,
-                       paste0(".//*[text()[contains(.,'", text,
-                              "')]]/ancestor::cell-line"))
+
+  if(length(text) == 1) {
+    xpath <- paste0(".//*[text()[contains(.,'", text,
+                    "')]]/ancestor::cell-line")
+  } else {
+    xpath <- paste0(".//*[text()[contains(.,'",
+                    paste(text, collapse = "') or contains(.,'"),
+                    "')]]/ancestor::cell-line")
+  }
+  xml2::xml_find_first(cellosaurus, xpath)
+
 }
 
 #' Find all matching cell-lines
@@ -59,19 +69,33 @@ cell_line_find_first <- function(cellosaurus, text) {
 #'   text. If there are no matches, the nodeset will be empty.
 #'
 #' @examples
+#' cellosaurus <- read_cellosaurus_xml("data/cellosaurus.xml")
+#'
 #' # Finding all the cell-lines that match "sapiens".
 #' # As of Cellosaurus v22.1 (2017-05-17) returned 69593 results.
 #' # Takes a minute or two on a reasonably quick modern laptop.
-#' cellosaurus <- read_cellosaurus_xml("data/cellosaurus.xml")
 #' cell_line_find_all(cellosaurus, "sapiens")
+#'
+#' # Supplying a vector of multiple search terms will do an "or" search:
+#' two_lines <- cell_line_find_all(cellosaurus, c("CVCL_E548", "CVCL_IW91"))
+#' two_species <- cell_line_find_all(cellosaurus, c("Mus musculus", "Cavia porcellus"))
 #'
 #' @export
 #'
 cell_line_find_all <- function(cellosaurus, text) {
-  xml2::xml_find_all(cellosaurus,
-                     paste0(".//*[text()[contains(.,'", text,
-                            "')]]/ancestor::cell-line"))
+
+  if(length(text) == 1) {
+    xpath <- paste0(".//*[text()[contains(.,'", text,
+                    "')]]/ancestor::cell-line")
+  } else {
+    xpath <- paste0(".//*[text()[contains(.,'",
+                    paste(text, collapse = "') or contains(.,'"),
+                    "')]]/ancestor::cell-line")
+  }
+  xml2::xml_find_all(cellosaurus, xpath)
+
 }
+
 
 #' Category of a cell-line
 #'
